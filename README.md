b5efa5be326dd68e8ff6b95041912f7fd3b7f700
Matteo Antony Mistretta <m.mistretta@reply.it>
2023-05-27 12:56:12 +0200

    Explain weak typing and variables

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 [CSS Base Tutorial](https://ryanstutorials.net/css-tutorial/css.php)
 [CSS Medium-Advanced Tutorials](https://htmldog.com/guides/css/)
+
+[JS-React Slides](https://docs.google.com/presentation/d/1ifBFn5293XAYoxksM5bCKg8DOVSNWgpci9WPdezSCVo/)
---
+++ js/02-variables.js
@@ -0,0 +1,18 @@
+// console.log(a, typeof a) // cannot access variable before declaration
+let a;
+console.log(a, typeof a);
+a = null;
+console.log(a, typeof a);
+a = 2;
+console.log(a, typeof a);
+a = NaN;
+console.log(a, typeof a);
+
+// let a = 3; // cannot redeclare variable
+let b = 3;
+
+const c = 4;
+// c = 5; // cannot reassign constant
+// const d; // declaring a constant without value makes no sense
+
+var d = 5; // NEVER use var
